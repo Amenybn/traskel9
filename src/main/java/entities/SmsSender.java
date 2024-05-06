f20be36ca3343ad32d9f5e0864ea19f4e2e4053a
Shaima Ben Nourddin <shaima.bennourddine@esprit.tn>
2024-05-06 23:06:41 +0200

    smsSender up

--- src/main/java/entities/SmsSender.java
+++ src/main/java/entities/SmsSender.java
@@ -1,22 +1,32 @@
 package entities;
 import com.twilio.Twilio;
-import com.twilio.converter.Promoter;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
-import java.net.URI;
-import java.math.BigDecimal;
-public class SmsSender {
-    public static final String ACCOUNT_SID = System.getenv("ACCOUNT_SID");
-    public static final String AUTH_TOKEN = System.getenv("AUTH_TOKEN");
 
-    public static void main(String[] args) {
+public class SmsSender {
+    private static final String ACCOUNT_SID = "ACCOUNT_SID";
+    private static final String AUTH_TOKEN = "AUTH_TOKEN";
+
+    /**
+     * Sends an SMS message using Twilio's Messaging API.
+     *
+     * @param toPhoneNumber The phone number to send the SMS to.
+     * @param messageBody The body of the SMS message.
+     */
+    private void sendSms(String toPhoneNumber, String messageBody) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
         Message message = Message.creator(
-                new PhoneNumber("+21651371144"), // receiver
-                new PhoneNumber("+13347589348"), // sender
-                "Bienvenue dans notre platforme Traskel!"
+                new PhoneNumber(toPhoneNumber),  // to
+                new PhoneNumber("+12512378308"), // from
+                messageBody
         ).create();
 
-        System.out.println(message.getSid());
+        System.out.println("Successfully sent message with SID: " + message.getSid());
+    }
+
+    public static void main(String[] args) {
+        SmsSender smsSender = new SmsSender();
+        smsSender.sendSms("+21651371144", "Bienvenue dans notre platforme Traskel!");
     }
 }
